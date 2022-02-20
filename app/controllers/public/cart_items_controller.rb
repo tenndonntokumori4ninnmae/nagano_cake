1c7daae4b17d638413117298477772f7a6171551
chibiriku <96232950+chibiriku@users.noreply.github.com>
2022-02-20 03:47:44 +0000

    order

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,19 @@
 class Public::CartItemsController < ApplicationController
+
   def index
+    @cart_items = current_customer.cart_items
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_item_path
+  end
+
+  private
+
+  def cart_item_params
+    params.permit(:item_id, :amount)
   end
 end
