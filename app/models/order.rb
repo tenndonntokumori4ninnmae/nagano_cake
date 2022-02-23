3437f9d074410b74084fa627ad163047699084bf
EC2 Default User <ec2-user@ip-172-31-39-48.ec2.internal>
2022-02-23 11:20:42 +0000

    kiku

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,6 @@ class Order < ApplicationRecord
   has_many :ordered_items,dependent: :destroy
   belongs_to :customer
 
-  validates :payment_method,:postal_code,:address,:name, 
-             presence: true
   validates :postal_code, length: {is: 7}, numericality: {only_integer: true}
 
 
