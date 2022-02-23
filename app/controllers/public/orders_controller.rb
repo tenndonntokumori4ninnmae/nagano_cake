0f367b9888d0a3d1fba6d7a7b78b5c48c19e558a
chibiriku <96232950+chibiriku@users.noreply.github.com>
2022-02-23 11:24:12 +0000

    121212

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -39,8 +39,9 @@ class Public::OrdersController < ApplicationController
        @ordered_item = OrderedItem.new(order_id: @order.id, item_id: cart_item.item_id, amount: cart_item.amount, price: cart_item.item.price)
        @ordered_item.save
         current_customer.cart_items.destroy_all
-      redirect_to thanks_path
       end
+      redirect_to thanks_path
+
     else
       render :index
     end
