97718c399dda0a710feee5c1f933e854e80343dc
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-02-23 07:50:54 +0000

    [add]レイアウト調整

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -2,6 +2,10 @@
 
 class Customers::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_up_path_for(resource)
+    customers_my_page_path
+  end  
 
 
   protected
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   def get_image
     unless image_id.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image_id.attached(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image_id.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
       image_id
   end
