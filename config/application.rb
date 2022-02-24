9f7d576beacb5f67fe34268c3e830d7f0a3e8752
EC2 Default User <ec2-user@ip-172-31-85-127.ec2.internal>
2022-02-24 05:04:33 +0000

    日本時間

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module NaganoCake
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
 
+    config.time_zone = 'Asia/Tokyo'
+
 
     # Configuration for the application, engines, and railties goes here.
     #
