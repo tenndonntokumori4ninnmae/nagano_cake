ab45f4472dac3e8c4931e45e353147d760e22d5f
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-02-24 08:43:16 +0000

    [update]ログインしていないとカートに追加できなくした

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,5 @@
 class Public::ItemsController < ApplicationController
-  before_action :authenticate_customer!, except: [:index]
+ 
 
   def index
     @items = Item.where(is_active: true).page(params[:page]).per(8)
